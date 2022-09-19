537368675204885f02cc76fff443d686fd2f3c34
Arpan Gulrajani <gulrajaniarpan@gmail.com>
2022-09-19 18:25:42 +0100

    Added criteria for punctuations removal

--- NLPAugmentation.py
+++ NLPAugmentation.py
@@ -8,6 +8,12 @@ import random
 from transformers import pipeline
 import pandas as pd
 
+pd.set_option('display.max_columns', None)
+
+# remove [SEP] and [CLS] tokens
+def remove_special_tokens(text):
+    return text.replace('[CLS]', '').replace('[SEP]', '').strip()
+
 unmasker = pipeline('fill-mask', model='bert-large-uncased')
 
 company_name = ["CEDAR HILL INDEPENDENT SCHOOL DISTRICT", "INMOBILIARIA OSNA SA DE CV",
@@ -27,6 +33,7 @@ def augment(company_name):
         new_mask_sent = ' '.join(new_text_list)
 
         augmented_text_list = unmasker(new_mask_sent)
+        print(augmented_text_list)
         augmented_text = [d['sequence'].upper() for d in augmented_text_list]
         all_aug_text.append(augmented_text)
 
@@ -34,7 +41,8 @@ def augment(company_name):
         new_text_list = orig_text_list[:rand_idx] + ['[MASK]'] + orig_text_list[rand_idx+1:]
         new_mask_sent = ' '.join(new_text_list)
         augmented_text_list = unmasker(new_mask_sent)
-        augmented_text = [d['sequence'].upper() for d in augmented_text_list]
+        augmented_text = [d['sequence'].upper() for d in augmented_text_list if d['token_str'] not in
+                          ['.', '|', '!', '?']]
         all_aug_text.append(augmented_text)
 
     # drop duplicates and flatten the list
